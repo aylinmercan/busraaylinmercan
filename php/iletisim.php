392da805349995b9deebc4c74e859570e95656a6
Büşra Aylin Mercan <aylin88s@hotmail.com>
2022-06-08 01:13:26 +0300

    add php

---
+++ php/iletisim.php
@@ -0,0 +1,41 @@
+<?php
+ 
+$ad     = $_POST["ad"];
+$soyad    = $_POST["soyad"];
+$email         = $_POST["email"];
+$mesaj        = $_POST["mesaj"];
+$adres        = "aylin88se@gmail.com"; // Buraya e-postanin gonderilecegi mail adresini yaziniz
+$konu        = "Iletisim Formu";
+$tarih        = date('Y-m-d');
+$ip_adresi    = $_SERVER['REMOTE_ADDR'];
+ 
+if(($ad=="") or ($tel =="") or ($email=="") or ($mesaj=="") or ($soyad=="")){
+ 
+echo "<center>Lutfen Ad Soyad, Telefon, E-Mail ve Mesaj alanlarini bos birakmayiniz.<br><a href=index.php>Geri don</a></center>";
+ 
+}
+else
+{
+ 
+$mesajveri.="ILETISIM FORMU MESAJI<br/><br/>";
+$mesajveri.="E-Mail:  ".$email."<br/>";
+$mesajveri.="Tarih:  ".$tarih."<br/>";
+$mesajveri.="IP Adresi   :".$ip_adresi."<br/>";
+$mesajveri.="Mesaj:  ".$mesaj;
+ 
+$mesajyolla = mail($adres, $konu, $mesajveri, "Content-type: text/html; charset=utf-8\r\n");
+ 
+if($mesajyolla)
+{
+ 
+echo "<center>Iletisim mailiniz bize ulasti, en kisa surede cevaplanacaktir. Ilginiz icin tesekkur ederiz.<br><a href=index.php>Anasayfa</a></center>";
+ 
+}
+else
+{
+ 
+echo "<center>E-Mail gonderilirken hata olustu! Lutfen daha sonra tekrar deneyiniz.</center>";
+ 
+}
+}
+ 
